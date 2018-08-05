99b162004916c4d6c4b8b1852cdfa37e97e5a479
KendallU <kendallunion@gmail.com>
2018-08-04 20:23:39 -0500

    Add project files.

---
+++ CG4-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CG4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number,");
+            Console.Write("Type number here:");
+            string userValue;
+            userValue = Console.ReadLine();
+
+            int xi = int.Parse(userValue);
+
+
+
+
+            for(int xi = 0; xi < 20; xi++) ;
+            {
+                if (userValue * xi == 0)
+            }
+
+            Console.WriteLine(xi);
+        }
+                       
+        }
+    }
+}
